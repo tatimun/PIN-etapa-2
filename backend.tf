f6a86f5ebcd92fc562c78a9759e7d52ce1d79627
tatianamunoz1 <tatianasmunoz@gmail.com>
2023-12-12 18:52:02 -0300

    TEST

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3"{
-    bucket                 = "mundose22"
+    bucket                 = "grupo2-1212"
     region                 = "us-east-1"
     key                    = "backend.tfstate"
     dynamodb_table         = "terraformstatelock"
